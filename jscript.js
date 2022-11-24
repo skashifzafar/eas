3e1af9797869be20d00add08ecd3d95208d5bd7e
skz <zafar.syedkashif@gmail.com>
2022-11-25 01:07:30 +0530

    basic hovering

--- jscript.js
+++ jscript.js
@@ -1,15 +1,30 @@
+function grid_input(){
+   size=parseInt(prompt('Please enter the size of the grid.','10'),10)
+}
+var size=10
 const container=document.querySelector('#container')
-for (var i=0;i<50;i++){
+const butt=document.createElement('input')
+butt.type='button'
+butt.value='Click to input size'
+const grid=document.createElement('div')
+grid.id='grid'
+const bdiv=document.createElement('div')
+bdiv.id='bdiv'
+bdiv.appendChild(butt)
+document.addEventListener('click',function(){grid_input()})
+console.log(size)
+container.appendChild(bdiv)
+container.appendChild(grid)
+for (var i=0;i<size;i++){
     const r=document.createElement('div')
-    for (var j=0;j<50;j++){
+    for (var j=0;j<size;j++){
         const b=document.createElement('div')
         const c=document.createElement('div')
         c.id=i+'-'+j
-        //c.textContent='Square '+i+'-'+j
         c.style.border='solid'
         c.style.borderWidth='1px';
         b.appendChild(c)
         r.appendChild(b)
     }
-    container.appendChild(r)
+    grid.appendChild(r)
 }
